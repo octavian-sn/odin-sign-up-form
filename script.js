d0f4a67580edd3a4f07953468e0fc26be82e4fab
Sulic Octavian <octavius1992@gmail.com>
2022-09-15 11:36:17 +0300

    Add password confirmation

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-const password = document.querySelector('#pass');
+const password = document.querySelector('#password');
 const confirmPass = document.querySelector('#confirm');
+const output = document.querySelector('output');
 
 confirmPass.addEventListener('input', checkMatch);
+password.addEventListener('input', passCheck);
+
 
 function checkMatch() {
     if (confirmPass.value !== password.value) {
-        confirmPass.style.backgroundColor = 'red';
+        confirmPass.style.borderColor = 'rgb(255, 80, 80)';
+        password.style.borderColor = 'rgb(255, 80, 80)';
+        output.value = "Passwords do not match"
     }
     if (confirmPass.value === password.value) {
-        confirmPass.style.backgroundColor = 'white';
+        confirmPass.style.borderColor = 'rgb(51, 175, 57)';
+        password.style.borderColor = 'rgb(51, 175, 57)';
+        output.value = '';
     }
+}
+
+function passCheck() {
+    if (confirmPass.value !== '') {checkMatch()};
 }
